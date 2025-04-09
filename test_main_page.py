f3084af014b05392418402197df258fb02a1fb22
nikontra <nikontra@yandex.ru>
2025-04-09 07:00:03 +0300

    Add is_element_present

--- test_main_page.py
+++ test_main_page.py
@@ -7,4 +7,10 @@ def test_guest_can_go_to_login_page(driver):
     page.open()
     page.go_to_login_page()
 
+def test_guest_should_see_login_link(driver):
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
+
+
 
